08e175eb52798664f3319efa300458766898883e
a21aitorna1 <a21aitorna@iessanclemente.net>
2025-10-29 19:27:46 +0100

    Docker sql creado, y conexión a base de datos

---
+++ app/backend/envs/dev/dev_env.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class DevConfig:
+    DEBUG = os.getenv('DEBUG_MODE', True)
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS', False)
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql+pymysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}"
+        f"@{os.getenv('MYSQL_HOST')}/{os.getenv('DATABASE_NAME')}"
+    )
+
+config = {
+    'dev': DevConfig
+}
+
+def get_database_config():
+    return {
+        'MYSQL_HOST': os.getenv('MYSQL_HOST'),
+        'MYSQL_USER': os.getenv('MYSQL_USER'),
+        'MYSQL_PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'DATABASE_NAME': os.getenv('DATABASE_NAME'),
+    }
--- app/backend/main.py
+++ app/backend/main.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from envs.dev.dev_env import config
+from database.db import init_app
 
 app = Flask(__name__)
+app.config.from_object(config['dev'])
+
+init_app(app)
 
 @app.route('/')
 def index():
     return "Comprobación contenedor backend correcto"
-
