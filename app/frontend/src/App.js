22b5adede133a7d839a1e4d929f34ed7d11d393a
a21aitorna1 <a21aitorna@iessanclemente.net>
2025-10-30 19:36:32 +0100

    Prueba tailwind

---
+++ app/frontend/src/App.js
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gradient-to-r from-purple-600 to-indigo-600 text-white text-4xl font-bold">
+      🎨 Tailwind funcionando perfectamente
+    </div>
+  );
+}
+export default App;
