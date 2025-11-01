3cdb90a041d8089babe6b5b68bc14418a44e5c97
a21aitorna1 <a21aitorna@iessanclemente.net>
2025-11-01 22:53:22 +0100

    swagger a .yml y cambio swag_from en login_routes

--- app/backend/features/user/routes/login_routes.py
+++ app/backend/features/user/routes/login_routes.py
@@ -1,14 +1,15 @@
+import os
 from flask import Blueprint, request
 from flasgger import swag_from
 from features.user.controller import login_controller
 
 login_routes = Blueprint('login_routes', __name__, url_prefix="/api")
-
-#Mantiene la definición de rutas y luego meterlas todas en main.py
+swagger_path = os.path.join(os.path.dirname(__file__), '../../../docs/users/login_swagger.yml'
+)
 def register_login_routes(app):
     app.register_blueprint(login_routes)
     
 @login_routes.post('/login')
-@swag_from('docs/users/login_swagger.json')
+@swag_from(swagger_path)
 def login_route():
     return login_controller()
