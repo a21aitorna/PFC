1388a1e9124d219d7a5ea3fdc1838e1213a74370
a21aitorna1 <a21aitorna@iessanclemente.net>
2025-11-02 22:52:24 +0100

    Cambio estilos

--- app/frontend/src/components/Card.jsx
+++ app/frontend/src/components/Card.jsx
@@ -1,6 +1,8 @@
 export default function Card({ icon: Icon, title, subtitle, children, className = "" }) {
   return (
-    <div className={`relative z-10 bg-white rounded-2xl shadow-xl p-8 w-[90%] max-w-md text-center ${className}`}>
+    <div
+      className={`relative z-10 bg-white rounded-2xl shadow-xl p-6 sm:p-8 w-[90%] sm:w-[80%] md:w-[60%] lg:w-[50%] xl:w-[40%] max-w-3xl text-center ${className}`}
+    >
       {/* Icono superior, opcional */}
       {Icon && (
         <div className="mx-auto bg-indigo-100 text-indigo-600 p-4 w-14 h-14 flex items-center justify-center rounded-full mb-4">
