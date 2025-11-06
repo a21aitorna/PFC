631d7742753d9562a20fe7846aea6431ef697519
a21aitorna1 <a21aitorna@iessanclemente.net>
2025-11-06 21:41:54 +0100

    Cambio header (logo y fuente)

--- app/frontend/src/components/Header.jsx
+++ app/frontend/src/components/Header.jsx
@@ -1,18 +1,34 @@
+import { useNavigate } from "react-router-dom";
+import logoAtenea from "../assets/images/logoAtenea.png";
+
 export default function Header() {
+  const navigate = useNavigate();
+
+  const handleReturnHome = () => {
+    navigate("/");
+  };
+
   return (
     <header className="absolute top-0 left-0 w-full flex items-center justify-start p-4 bg-black/40 backdrop-blur-md">
-        <div className="flex items-center space-x-2 text-white">
-          <div className="bg-indigo-500 rounded-md p-2">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none"
-              viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 5h16M4 10h16M4 15h16" />
-            </svg>
-          </div>
-          <div>
-            <p className="font-semibold">Proyecto Atenea</p>
-            <p className="text-xs text-gray-300">Portal de Acceso</p>
-          </div>
+      <div
+        data-testid="returnMainPage"
+        onClick={handleReturnHome}
+        className="flex items-center space-x-3 text-white cursor-pointer select-none"
+      >
+        {/* Icono dentro del recuadro */}
+        <div className="bg-indigo-500 rounded-xl p-2 flex items-center justify-center shadow-md h-12 w-12">
+          <img
+            src={logoAtenea}
+            alt="Logo Atenea"
+            className="h-8 w-8 object-contain filter invert brightness-0"
+          />
         </div>
+
+        {/* Texto al lado derecho del icono */}
+        <div className="flex flex-col justify-center leading-tight">
+          <p className="font-gelio text-3xl">Proyecto Atenea</p>
+        </div>
+      </div>
     </header>
   );
 }
