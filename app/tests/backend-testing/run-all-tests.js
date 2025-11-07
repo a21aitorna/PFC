7b7501343f4b5991812e487ea62a82d09ac0d251
a21aitorna1 <a21aitorna@iessanclemente.net>
2025-11-07 23:22:49 +0100

    Método para lanzar todos los test de colecciones seguidos

---
+++ app/tests/backend-testing/run-all-tests.js
@@ -0,0 +1,31 @@
+const newman = require('newman');
+
+const collections = [
+    {file: 'collections/API_Login_Tests.json', report: 'reports/login.html'},
+    {file: 'collections/API_Register_Tests.json', report: 'reports/register.html'}
+];
+//ir añadiendo en el array todas las colecciones
+
+const environment = 'environments/local.env.json';
+
+(async () => {
+  for (const c of collections) {
+    console.log(`Ejecutando colección: ${c.file}`);
+    await new Promise((resolve, reject) => {
+      newman.run({
+        collection: c.file,
+        environment,
+        reporters: ['htmlextra'],
+        reporter: { htmlextra: { export: c.report } }
+      }, (err) => {
+        if (err) reject(err);
+        else {
+          console.log(`✅ Finalizada: ${c.file}`);
+          resolve();
+        }
+      });
+    });
+  }
+})();
+
+// node run-all-tests.js -> comando para lanzar todos los tests
