ddba2c5be4f7cc4e8b1b2136b4cec535d7c6c9d8
a21aitorna1 <a21aitorna@iessanclemente.net>
2025-11-20 21:33:24 +0100

    Bloquear y desbloquear usuarios, desbloquar usuarios automáticamente treas 3 días

--- app/backend/main.py
+++ app/backend/main.py
@@ -22,7 +22,7 @@ from features.book.routes.book_routes import register_book_routes
 from features.admin.routes.admin_routes import register_admin_routes
 from seed import seed_roles, seed_test_user, seed_admin_user
 from utils.jwt_decorator import register_jwt_callbacks
-from tasks.automatic_unblock_users import automatic_unblock_users
+from tasks.automatic_tasks import automatic_unblock_users
 
 app = Flask(__name__)
 app.config.from_object(config['dev'])
--- app/backend/tasks/automatic_tasks.py
+++ app/backend/tasks/automatic_tasks.py
@@ -1,28 +1,17 @@
-import logging
 from datetime import date, timedelta
 from database.db import db
 from dao.persona_dao import Persona
-
-logging.basicConfig(
-    filename='/var/log/unblock_users.log',
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s'
-)
 
 def automatic_unblock_users():
     today = date.today()
     three_days_ago = today - timedelta(days=3)
     
     expired_users = Persona.query.filter(Persona.is_blocked==True, Persona.block_date<= three_days_ago).all()
-    logging.info(f"Usuarios con bloqueo expirado encontrados: {len(expired_users)}")
-
     for user in expired_users:
         user.is_blocked = False
         user.block_date = None
-        logging.info(f"Usuario desbloqueado automáticamente: {user.username}")
 
     if expired_users:
         db.session.commit()
-        logging.info("Desbloqueo automático completado correctamente.")
     else:
-        logging.info("No había usuarios para desbloquear.")
+        print("No había usuarios para desbloquear.")
